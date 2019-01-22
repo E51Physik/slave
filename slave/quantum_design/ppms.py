587abb72caf76f8935c95ecf2bd9205e96bb0aab
E51Physik <46356627+E51Physik@users.noreply.github.com>
2019-01-22 16:21:26 +0100

    Update ppms.py

--- slave/quantum_design/ppms.py
+++ slave/quantum_design/ppms.py
@@ -127,7 +127,6 @@ STATUS_LINK = {
 }
 
 from slave.quantum_design import qd_magnet
-#magnet = qd_magnet.HMPSU()
 
 class PPMS(IEC60488):
     """A Quantum Design Model 6000 PPMS.
@@ -324,8 +323,7 @@ class PPMS(IEC60488):
             resp_term=';'
         )
         super(PPMS, self).__init__(transport, protocol)
-        #magnet = qd_magnet.HMPSU()
-        self.advisory_number = Command(('ADVNUM?', Integer(min=0, max=999)))
+        #self.advisory_number = Command(('ADVNUM?', Integer(min=0, max=999)))
         self.chamber = Command(
             'CHAMBER?',
             'CHAMBER',
